6447c50ed58a18bb3287774675bb64246e101ed0
vcreatorv <valery.nagapetyan@yandex.ru>
2024-10-01 17:25:22 +0000

    deleted unused import

--- src/main/java/com/valer/rip/lab1/controllers/ConnectionRequestController.java
+++ src/main/java/com/valer/rip/lab1/controllers/ConnectionRequestController.java
@@ -1,7 +1,5 @@
 package com.valer.rip.lab1.controllers;
 
-import java.util.Optional;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
